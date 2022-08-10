dd64b71f29870f9385263fc1d3fad73859aa68bf
David Bielik <davidbielik@gmail.com>
2022-08-10 16:11:35 -0400

    added friend requests controller

---
+++ app/controllers/friend_requests_controller.rb
@@ -0,0 +1,20 @@
+class FriendRequestsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_friend_request, except: :create
+  
+    def create
+      friend_request = FriendRequest.create(requester_id: current_user.id, recipient_id: params[:recipient_id])
+      if friend_request.save
+        redirect_back fallback_location: user_url(current_user), notice: 'Friend Request sent!'
+      else
+        redirect_back fallback_location: user_url(current_user), alert: "Sorry, your request could not be completed. (#{friend_request.errors.full_messages.join(', ')}.)"
+      end
+    end
+  
+    private
+  
+    def set_friend_request
+      @friend_request = FriendRequest.find(params[:id])
+    end
+  end
+end
