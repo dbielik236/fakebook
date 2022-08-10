245bc4a209e175a1e5b188c9257a2812b7892749
David Bielik <davidbielik@gmail.com>
2022-08-10 16:33:55 -0400

    updated user model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
+  has_many :posts,
+           dependent: :destroy
+
+  has_many :friend_requests,
+           foreign_key: :requester_id,
+           dependent: :destroy
+
+  has_many :requested_friends,
+           through: :friend_requests,
+           source: :recipient
+
+  has_many :friend_invitations,
+           foreign_key: :recipient_id,
+           class_name: :FriendRequest,
+           dependent: :destroy
+
+  has_many :requesting_friends,
+           through: :friend_invitations,
+           source: :requester
 end
