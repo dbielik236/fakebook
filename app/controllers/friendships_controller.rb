c9b6d0deec34e248fd170f63c68c03512030c93f
David Bielik <davidbielik@gmail.com>
2022-08-14 17:54:19 -0400

    destroys friend request when creating frienship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,7 @@ class FriendshipsController < ApplicationController
     def create
         friendship = Friendship.create(friend_a_id: current_user.id, friend_b_id: params[:friend_b_id])
         if friendship.save
-          
+          FriendRequest.destroy_by(requester_id: params[:friend_b_id])
           redirect_to users_path, alert: "Friendship added!"
         else
           redirect_back fallback_location: user_url(current_user), alert: "Sorry, your request could not be completed. (#{friendship.errors.full_messages.join(', ')}.)"
