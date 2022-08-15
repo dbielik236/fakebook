8f3491bc81b349d0772a3c54a399f7efa4863663
David Bielik <davidbielik@gmail.com>
2022-08-15 14:39:15 -0400

    added user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show edit update destroy ]
     before_action :authenticate_user!
 
     def index
         @users = User.all
 
+    end
+
+    def show
+       
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
     end
 
 end
