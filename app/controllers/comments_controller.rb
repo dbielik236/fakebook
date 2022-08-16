b3f0ae25e2d0acec6de48299722e19ee6a961f32
David Bielik <davidbielik@gmail.com>
2022-08-16 15:37:16 -0400

    comments correctly save and redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,26 @@
 class CommentsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def new
-      @post = Post.find(params[:post_id])
-    end
+  def new
+    @post = Post.find(params[:post_id])
+  end
 
-    def create
-      @comment = current_user.comments.build(comment_params)
-    end
-
-    def destroy
-      comment = Comment.find(params[:id])
-      comment.destroy
-      redirect_to url
-    end
+  def create
+    @comment = current_user.comments.build(comment_params)
     
-    private
-    
-    def comment_params
-      params.require(:comment).permit(:body, :commentable_id, :commentable_type)
-    end
+    flash.alert = "Error: #{@comment.errors.full_messages.join}" unless @comment.save
+    redirect_to root_path
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+  end
 end
