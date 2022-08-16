e912af16862edeed12d7bfa750372f111e190d45
David Bielik <davidbielik@gmail.com>
2022-08-16 10:26:38 -0400

    created comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+      @post = Post.find(params[:post_id])
+    end
+
+    def create
+      comment = current_user.comments.build(comment_params)
+    end
+
+    def destroy
+      comment = Comment.find(params[:id])
+      comment.destroy
+      redirect_to url
+    end
+    
+    private
+    
+    def comment_params
+      params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+    end
+end
