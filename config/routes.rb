b8214c396b096383419c170928c528db8ed97482
David Bielik <davidbielik@gmail.com>
2022-08-16 10:22:36 -0400

    added routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :friend_requests
   resources :friendships
+  resources :comments
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
